aa2cd19885848f577c91253cfa69e400855d1766
SouleyDIA <Souleymane.dia@edu.devinci.fr>
2022-10-09 19:50:13 +0200

    Added FilteredLogger Class

--- src/main/java/fr/lernejo/logger/ContextualLogger.java
+++ src/main/java/fr/lernejo/logger/ContextualLogger.java
@@ -3,17 +3,17 @@ package fr.lernejo.logger;
 
 public class ContextualLogger implements Logger {
 
-    private final Logger logger;
+    private final FilteredLogger filteredLogger;
     private final String className;
 
-    public ContextualLogger(Logger logger, String className) {
-        this.logger = logger;
+    public ContextualLogger(FilteredLogger filteredLogger, String className) {
+        this.filteredLogger = filteredLogger;
         this.className = className;
     }
 
     @Override
     public void log(String message) {
-        logger.log("[" + java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) + "] [" + className + "] " + message);
+        filteredLogger.log("[" + java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) + "] [" + className + "] " + message);
     }
 
 }
