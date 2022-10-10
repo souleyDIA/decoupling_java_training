6d8136eed8f0b506620fc896f9619858e5785312
SouleyDIA <Souleymane.dia@edu.devinci.fr>
2022-10-10 10:47:54 +0200

    relative path file test

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -7,7 +7,7 @@ public class LoggerFactory {
     public static Logger getLogger(String className){
         Predicate<String> filter = s -> !s.contains("simulation");
         Logger consoleLogger = new ConsoleLogger();
-        Logger fileLogger = new FileLogger("src/test/java/ComputerPlayerLogTest.txt");
+        Logger fileLogger = new FileLogger("./ComputerPlayerLogTest.txt");
         Logger compositeLogger = new CompositeLogger(consoleLogger, fileLogger);
         Logger contextualLogger = new ContextualLogger(className, compositeLogger);
         return new FilteredLogger(contextualLogger, filter); 
